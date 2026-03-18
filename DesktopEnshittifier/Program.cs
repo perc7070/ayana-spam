417840245ed8e978cf40bb29eb374bce8778b35f
perc7070 <alistairealternate@gmail.com>
2026-03-19 01:11:06 +1100

    no more fork bomb cos i nuked my pc

--- DesktopEnshittifier/Program.cs
+++ DesktopEnshittifier/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Diagnostics;
 using System.Reflection.Metadata;
 using System.Drawing;
 
 class Enshittify
-{
+{ // woah only 50 processes??!! no more fork bomb!!!!!!!
     static void Main()
     {
 
@@ -23,7 +23,7 @@ class Enshittify
         ayanaBitmap.Save(pathToAyana);
         // this is all straight shit idgaf tho :P
 
-        Console.WriteLine("Type 'start' to Start"); // this bullshit doesnt even do anything i hateit
+        Console.WriteLine("you may lose a bunch of unsaved stuff save it and come back and be prepared to have to restart ur pc. \ntype start to start"); // this bullshit doesnt even do anything i hateit
 
         if (Console.ReadLine() == "start")
         {
@@ -32,8 +32,9 @@ class Enshittify
                 using (Process openAyaners = new Process())
                 {
                     openAyaners.StartInfo.FileName = pathToAyana;
-
-                    while (true)
+                    openAyaners.StartInfo.UseShellExecute = true;
+                    Console.WriteLine(pathToAyana + "\n");
+                    for (int i = 0; i < 10; i++)
                     {
                         openAyaners.Start();
                     }
@@ -41,14 +42,15 @@ class Enshittify
             }
             catch (Exception e)
             {
-                Console.WriteLine("sorry" + e.Message);
+                Console.WriteLine("sorry " + e.Message);
                 using (Process openAyaners = new Process())
                 {
+                    openAyaners.StartInfo.Arguments = pathToAyana;
                     openAyaners.StartInfo.FileName = @"C:\Program Files\Mozilla Firefox\firefox.exe";
                     //this is dumb i am dumb
-                    openAyaners.StartInfo.Arguments = pathToAyana;
+                    openAyaners.StartInfo.UseShellExecute = true;
 
-                    while (true)
+                    for (int i = 0; i < 50; i++)
                     {
                         openAyaners.Start();
                     }
